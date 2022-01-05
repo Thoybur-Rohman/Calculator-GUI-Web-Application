34292bf250e3c8619199c4307b52e955c8fe2a25
Thoybur Rohman <73062879+Thoybur-Rohman@users.noreply.github.com>
2022-01-05 03:59:45 +0000

    Add files via upload

--- CalCulator/script.js
+++ CalCulator/script.js
@@ -12,8 +12,39 @@ function printOutput(num){
         document.getElementById("output-value").innerText= getFormattedNumber(num);
 }
 function getFormattedNumber(num){
-    const n = Number(num)
-    const value = n.toLocaleString("en");
+    let n = Number(num)
+    let value = n.toLocaleString("en");
     return value;
 }
-printOutput("35655613");
+function reverseNumberFormat(num){
+    return Number(num.replace(/,/g,''));
+}
+
+const operator = document.getElementsByClassName("operator");
+for (let i = 0 ; i < operator.length ;i++){
+    operator[i].addEventListener('click',function(){
+        if (this.id == "clear"){
+            printHistory("");
+            printOutput("");
+        }
+        if (this.id = "backspace"){
+            let output=reverseNumberFormat(getOutput()).toString();
+            if (output){
+                 output = output.substr(0,output.length-1)
+                printOutput(output);
+
+            }
+
+        }
+    });
+}
+var number = document.getElementsByClassName("number");
+for(let i =0; i<number.length; i++){
+    number[i].addEventListener('click',function(){
+        var output=reverseNumberFormat(getOutput());
+        if(output!=NaN){ //if output is a number
+            output=output+this.id;
+            printOutput(output);
+        }
+    });
+}
